6fe0a37ab0d40e02f5fc065940d3fc4334056352
arnaugt25 <agt.arnau2525@gmail.com>
2024-11-18 16:33:23 +0100

    emeset y docker

---
+++ src/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/** 
+ * Fitxer de configuració de l'aplicació.
+ * */ 
+
+$config = [
+    "db" => [
+        "host" => "mysql",
+        "user" => "root",
+        "password" => "12345",
+        "database" => "dbweb"
+    ]
+];
