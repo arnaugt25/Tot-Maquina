218c1bbc45ddefb13cce58a9b55c9f33fb3d573b
arnaugt25 <agt.arnau2525@gmail.com>
2024-12-13 16:45:01 +0100

    .

--- App/models/Machine.php
+++ App/models/Machine.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 class Machine extends Db
 {
-    public function addMachinea($data)
+    public function addMachine($data)
     {
         $query = "INSERT INTO machine (model, created_by, serial_number, installation_date, coordinates,  image ) 
 
