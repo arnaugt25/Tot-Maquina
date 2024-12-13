1380caa63644b44da379d8665204ea2298d60858
arnaugt25 <agt.arnau2525@gmail.com>
2024-12-13 16:03:43 +0100

    arreglos

--- App/Container.php
+++ App/Container.php
@@ -25,16 +25,14 @@ class Container extends EmesetContainer {
             );
         };
 
-        $this["Machine"] = function ($c) {
-            $db = $c->get("db");
-            $config = $c->get("config");
-            $machine = new \App\Models\Machine(
+        $this["Machine"] = function ($container) {
+            $config = $container->get("config");
+            return new \App\Models\Machine(
                 $config["db"]["user"],
                 $config["db"]["pass"],
                 $config["db"]["name"],
                 $config["db"]["host"]
             );
-            return $machine;
         };
 
         $this["maintenance"] = function ($c) {
