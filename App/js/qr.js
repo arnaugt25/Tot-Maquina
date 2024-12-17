652ee72bf50f8ed4958500f2a4252591ffb75aab
arnaugt25 <agt.arnau2525@gmail.com>
2024-12-17 16:30:33 +0100

    .

--- App/js/qr.js
+++ App/js/qr.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() { 
     const urlParams = new URLSearchParams(window.location.search);
     const machineId = urlParams.get('machine_id');
     
