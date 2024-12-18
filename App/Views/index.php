8e20a1bb619608ff50c2df5057c2c0bee93e5c0c
caroDf <cdenhoff@cendrassos.net>
2024-12-18 16:20:03 +0100

    Cambios

--- App/Views/index.php
+++ App/Views/index.php
@@ -84,13 +84,13 @@
         <!-- Logo and name -->
         <div class="flex items-center space-x-4">
           <img src="/uploads/images/logototmaquina.png" alt="Logo" class="h-20 transition-transform hover:scale-105">
-          <span class="text-xl font-bold text-[#5DA6C3]">Tot rsdfghjhgfghjkljhgfbjkmMaquina</span>
+          <span class="text-xl font-bold text-[#5DA6C3]">Tot Maquina</span>
         </div>
         <!--Enlaces de navegación (Navigation links) -->
         <div class="hidden md:block">
           <div class="flex items-center space-x-8">
             <a href="/" class="relative group px-3 py-2 text-[#C1D1D8] hover:text-white transition-colors duration-300">
-              <i class="fa-solid fa-house"></i> Inicio
+              <i class="fa-solid fa-house"></i> Inicihgfhgjko
               <span class="absolute bottom-0 left-0 w-full h-0.5 bg-[#5DA6C3] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
             </a>
             <a href="/addlist" class="relative group px-3 py-2 text-[#C1D1D8] hover:text-white transition-colors duration-300">
